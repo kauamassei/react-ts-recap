b1a76b0ec384ea11cdcba88cdb378c7bf5081c6d
kauamassei <gameplayultra11@gmail.com>
2025-08-20 00:22:51 -0300

    feat: add my first component

---
+++ 1_fundamentos/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Welcome from './components/Welcome'
+
+function App() {
+
+
+  return (
+    <>
+      <Welcome />
+    </>
+  )
+}
+
+export default App
---
+++ 1_fundamentos/src/components/Welcome.tsx
@@ -0,0 +1,17 @@
+
+
+const Welcome = () => {
+    const name = 'kaua'
+  return (
+    <>
+    <div>
+        <p className="text-sm font-xl">
+            Hello react meu nome é {name}
+        </p>
+    </div>
+    
+    </>
+  )
+}
+
+export default Welcome
