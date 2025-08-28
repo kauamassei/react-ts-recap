c3622c4ecfc5d0213804528663514d03910f5619
kauamassei <gameplayultra11@gmail.com>
2025-08-28 00:39:47 -0300

    feat: add Timer component

---
+++ 2_hooks/src/components/Timer.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+const Timer = () => {
+  const [segundos, setSegundos] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSegundos((prevSegundos) => prevSegundos + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+  return <div>Timer: {segundos}</div>;
+};
+
+export default Timer;
