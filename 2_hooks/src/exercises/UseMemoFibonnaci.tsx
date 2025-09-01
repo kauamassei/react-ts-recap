d6ceff62ef132f890396524f79a1347136152331
kauamassei <gameplayultra11@gmail.com>
2025-09-01 01:42:46 -0300

    feat: add useMemoFibonnaci exercise

---
+++ 2_hooks/src/exercises/UseMemoFibonnaci.tsx
@@ -0,0 +1,28 @@
+import { useMemo } from "react";
+
+interface Calculator {
+  num: number;
+}
+
+interface UseMemoFibonnaciProps {
+  functionNumber: number;
+}
+
+const fibonnaci = ({ num }: Calculator): number => {
+  if (num <= 1) return num;
+  return fibonnaci({ num: num - 1 }) + fibonnaci({ num: num - 2 });
+};
+
+const UseMemoFibonnaci = ({ functionNumber }: UseMemoFibonnaciProps) => {
+  const fibResult = useMemo(() => fibonnaci({ num: functionNumber }), [functionNumber]);
+
+  return (
+    <div>
+      <p>
+        Fibonacci de {functionNumber} é {fibResult}
+      </p>
+    </div>
+  );
+};
+
+export default UseMemoFibonnaci;
