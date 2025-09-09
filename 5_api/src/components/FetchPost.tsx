a96f6ad1b355a470f937330cee117147c7007991
kauamassei <gameplayultra11@gmail.com>
2025-09-09 01:35:30 -0300

    feat: add FetchPost with JSONPlaceholder API

--- 5_api/src/components/FetchPost.tsx
+++ 5_api/src/components/FetchPost.tsx
@@ -1,8 +1,48 @@
+import { useState, useEffect } from "react";
 
-const FetchPost = () => {
-  return (
-    <div>FetchPost</div>
-  )
+interface Post {
+  id: number;
+  title: string;
+  body: string;
 }
 
-export default FetchPost
+const FetchPost = () => {
+  const [posts, setPost] = useState<Post[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+
+        const data = await response.json();
+
+        setPost(data);
+      } catch (error) {
+        setError(String(error));
+      }
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <>
+      <h1>Fetch API</h1>
+      <div>
+        {error ? (
+          <p>Houve um error {error}</p>
+        ) : (
+          posts.map((post) => (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          ))
+        )}
+      </div>
+    </>
+  );
+};
+
+export default FetchPost;
