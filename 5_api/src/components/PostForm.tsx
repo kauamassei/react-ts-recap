1eba192eb7e2d861d1ad6ddb53b548ac68dabb26
kauamassei <gameplayultra11@gmail.com>
2025-09-10 01:54:23 -0300

    feat: add POST on API with axios

--- 5_api/src/components/PostForm.tsx
+++ 5_api/src/components/PostForm.tsx
@@ -1,29 +1,50 @@
 import { useState } from "react";
 import axios from "axios";
+import type { Posts } from "./PostManager";
 
-const PostForm = () => {
+const URL = "https://jsonplaceholder.typicode.com/posts";
+
+interface AddPost {
+  onSuccess: (post: Posts, operation:  "add" | "edit") => void;
+}
+
+const PostForm = ({ onSuccess }: AddPost) => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const newPost = { title, body, userId: 1 };
+
+    try {
+      const response = await axios.post(URL, newPost);
+
+      onSuccess(response.data, "add");
+    } catch (error) {
+      console.log("Erro ao enviar postagem: ", error);
+    }
   };
   return (
     <>
       <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={title}
-          placeholder="Digite o título"
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </form>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={title}
+            placeholder="Digite o título"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+          <textarea
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            placeholder="Digite o conteúdo..."
+          ></textarea>
+
+          <button type="submit">Enviar</button>
+        </form>
       </div>
-      <div>
-        <textarea value={body} onChange={(e) => setBody(e.target.value)} placeholder="Digite o conteúdo..."></textarea>
-      </div>
-      <button type="submit">Enviar</button>
     </>
   );
 };
--- 5_api/src/components/PostManager.tsx
+++ 5_api/src/components/PostManager.tsx
@@ -2,16 +2,24 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PostForm from "./PostForm";
 
-interface Posts {
+export interface Posts {
   id: number;
   title: string;
   body: string;
 }
 
+
 const URL = "https://jsonplaceholder.typicode.com/posts";
 
 const PostManager = () => {
   const [posts, setPosts] = useState<Posts[]>([]);
+
+
+  const handleSuccess = (post: Posts , operation: 'add' | "edit") => {
+    if(operation === 'add') {
+        setPosts((currentPosts) => [post, ...currentPosts])
+    }
+  }
 
   useEffect(() => {
     fetchPosts();
@@ -29,13 +37,13 @@ const PostManager = () => {
   return (
     <>
     <h1>Gerenciar posts</h1>
-    <PostForm />
+    <PostForm onSuccess={handleSuccess} />
       <h2>Postagens</h2>
       {posts.map((post) => (
         <div key={post.id}>
           <h3>{post.title}</h3>
           <p>{post.body}</p>
-          <button onClick={}>Editar</button>
+          <button>Editar</button>
         </div>
       ))}
     </>
