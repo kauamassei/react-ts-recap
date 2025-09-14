4944a245cbf353a9fb55c9456a77e96fff76831e
kauamassei <gameplayultra11@gmail.com>
2025-09-13 21:44:16 -0300

    feat: add handleEdit and handleCancelEdit functions

--- 5_api/src/components/PostManager.tsx
+++ 5_api/src/components/PostManager.tsx
@@ -8,18 +8,28 @@ export interface Posts {
   body: string;
 }
 
-
 const URL = "https://jsonplaceholder.typicode.com/posts";
 
 const PostManager = () => {
   const [posts, setPosts] = useState<Posts[]>([]);
+  const [selectedPost, setSelectedPost] = useState<Posts | null>(null);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
 
-
-  const handleSuccess = (post: Posts , operation: 'add' | "edit") => {
-    if(operation === 'add') {
-        setPosts((currentPosts) => [post, ...currentPosts])
+  const handleSuccess = (post: Posts, operation: "add" | "edit") => {
+    if (operation === "add") {
+      setPosts((currentPosts) => [post, ...currentPosts]);
     }
-  }
+  };
+
+  const handleEdit = (post: Posts) => {
+    setSelectedPost(post);
+    setIsEditing(true);
+  };
+
+  const handleCancelEdit = () => {
+    setSelectedPost(null);
+    setIsEditing(false);
+  };
 
   useEffect(() => {
     fetchPosts();
@@ -36,14 +46,15 @@ const PostManager = () => {
 
   return (
     <>
-    <h1>Gerenciar posts</h1>
-    <PostForm onSuccess={handleSuccess} />
+      <h1>Gerenciar posts</h1>
+      <PostForm onSuccess={handleSuccess} />
+      {isEditing && <button onClick={handleCancelEdit}>Cancelar edicao</button>}
       <h2>Postagens</h2>
       {posts.map((post) => (
         <div key={post.id}>
           <h3>{post.title}</h3>
           <p>{post.body}</p>
-          <button>Editar</button>
+          <button onClick={() => handleEdit(post)}>Editar</button>
         </div>
       ))}
     </>
