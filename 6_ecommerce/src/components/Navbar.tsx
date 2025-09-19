b88cabd5d6280e970295cce99c1b43aecc8c0968
kauamassei <gameplayultra11@gmail.com>
2025-09-19 02:34:21 -0300

    styles: add css

--- 6_ecommerce/src/components/Navbar.tsx
+++ 6_ecommerce/src/components/Navbar.tsx
@@ -1,21 +1,16 @@
-import { Link, Routes, Route } from "react-router-dom"
-import Home from "./Home"
-import Cart from "./Cart"
+import { Link,  } from "react-router-dom";
+
 
 const Navbar = () => {
   return (
     <>
-    <div>
-        <Link to='/'>Catálogo</Link>
-        <Link to='/cart'>Carrinho</Link>
+      <div className="nav-container">
+        <Link to="/">Catálogo</Link>
+        <Link to="/cart">Carrinho</Link>
+      </div>
 
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/cart" element={<Cart />}/>
-        </Routes>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
