dcafccb7009e3328cf3ae432eeaf37bdd567ae9a
kauamassei <gameplayultra11@gmail.com>
2025-09-19 02:31:58 -0300

    fix: add routes in App.tsx

--- 6_ecommerce/src/App.tsx
+++ 6_ecommerce/src/App.tsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Catalog from "./pages/Catalog";
+import Cart from "./pages/Cart";
 
 function App() {
-  return <>
-  <Navbar />
-  </>;
+  return (
+    <div>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Catalog />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
