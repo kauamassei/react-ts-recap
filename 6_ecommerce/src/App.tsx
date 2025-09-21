43008ad025ab3afa6dc5f588656452bd7403acf3
kauamassei <gameplayultra11@gmail.com>
2025-09-21 03:54:02 -0300

    feat: add Toast of success and info message

--- 6_ecommerce/src/App.tsx
+++ 6_ecommerce/src/App.tsx
@@ -7,6 +7,10 @@ import type { Products } from "./components/Product";
 import { useState } from "react";
 import Finished from "./pages/Finished";
 
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   const [cartItems, setCartItems] = useState<
     { product: Products; quantity: number }[]
@@ -14,15 +18,19 @@ function App() {
 
   const handleAddCart = (product: Products, quantity: number) => {
     setCartItems((prevItems) => {
-      const itemExists = prevItems.find((item) => item.product.id === product.id);
+      const itemExists = prevItems.find(
+        (item) => item.product.id === product.id
+      );
 
       if (itemExists) {
+        toast.info(`Quantidade do item ${product.name} atualizada.`)
         return prevItems.map((item) =>
           item.product.id === product.id
             ? { ...item, quantity: item.quantity + quantity }
             : item
         );
       } else {
+        toast.success(`${product.name} adicionado com sucesso!`)
         return [...prevItems, { product, quantity }];
       }
     });
@@ -33,10 +41,18 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Catalog onAddToCart={handleAddCart} />} />
-          <Route path="/cart" element={<Cart cartItems={cartItems}  />} />
+          <Route path="/cart" element={<Cart cartItems={cartItems} />} />
           <Route path="/finished" element={<Finished />} />
         </Routes>
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+      />
     </div>
   );
 }
