9782866d9d0bb688d2290d84ca411feda1bed73e
kauamassei <gameplayultra11@gmail.com>
2025-09-28 01:19:58 -0300

    fix exercise 1 and add exercise 2

--- 7_typescript/dist/exercises.js
+++ 7_typescript/dist/exercises.js
@@ -1,8 +1,18 @@
 // Exercício 1
+const describeVehicle = (vehicle) => {
+    console.log(`O veículo é da marca: ${vehicle.make}, modelo: ${vehicle.model} e foi fabricado em ${vehicle.year} `);
+};
 const Vehicle = {
     make: "BMW",
     model: "X5",
     year: 2018,
 };
-console.log(Vehicle);
+describeVehicle(Vehicle);
 // Exercício 2
+function firstElement(arr) {
+    return arr[0];
+}
+const newNumbers = [1, 2, 3];
+console.log(firstElement(newNumbers));
+const fruits = ["Morango", "Banana", "Uva"];
+console.log(firstElement(fruits));
--- 7_typescript/src/exercises.ts
+++ 7_typescript/src/exercises.ts
@@ -6,14 +6,31 @@ interface VehicleProps {
   year: number;
 }
 
+const describeVehicle = (vehicle: VehicleProps): void => {
+  console.log(
+    `O veículo é da marca: ${vehicle.make}, modelo: ${vehicle.model} e foi fabricado em ${vehicle.year} `
+  );
+};
+
 const Vehicle: VehicleProps = {
   make: "BMW",
   model: "X5",
   year: 2018,
 };
 
-console.log(Vehicle);
+describeVehicle(Vehicle);
 
 // Exercício 2
 
+function firstElement<E>(arr: E[]): E {
+  return arr[0];
+}
+
+const newNumbers = [1, 2, 3];
+
+console.log(firstElement(newNumbers));
+
+const fruits = ["Morango", "Banana", "Uva"];
+
+console.log(firstElement(fruits));
 
