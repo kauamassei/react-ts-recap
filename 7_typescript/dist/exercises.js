6b6c9dfd4535accc708d2a02ddd905e236428ede
kauamassei <gameplayultra11@gmail.com>
2025-09-28 01:08:17 -0300

    feat: add exercise 1

---
+++ 7_typescript/dist/exercises.js
@@ -0,0 +1,8 @@
+// Exercício 1
+const Vehicle = {
+    make: "BMW",
+    model: "X5",
+    year: 2018,
+};
+console.log(Vehicle);
+// Exercício 2
---
+++ 7_typescript/src/exercises.ts
@@ -0,0 +1,19 @@
+// Exercício 1
+
+interface VehicleProps {
+  make: string;
+  model: string;
+  year: number;
+}
+
+const Vehicle: VehicleProps = {
+  make: "BMW",
+  model: "X5",
+  year: 2018,
+};
+
+console.log(Vehicle);
+
+// Exercício 2
+
+
