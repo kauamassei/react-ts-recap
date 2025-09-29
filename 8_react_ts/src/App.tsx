f9d6d6797d317333f2e9b7ad005da352767ddf4f
kauamassei <gameplayultra11@gmail.com>
2025-09-29 01:58:36 -0300

    feat: add TextInput component

--- 8_react_ts/src/App.tsx
+++ 8_react_ts/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Counter from "./components/Counter";
 import Greeting from "./components/Greeting";
+import TextInput from "./components/TextInput";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>React com TS</h1>
       <Greeting name="Kauã" />
       <Counter />
+      <TextInput />
     </>
   );
 }
