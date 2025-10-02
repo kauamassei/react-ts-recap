5ec53e959aa802a81e82ffe2f24f485d138f7e29
kauamassei <gameplayultra11@gmail.com>
2025-10-02 01:47:55 -0300

    style: list-style: none

--- 9_exercises/src/components/ShoppingList.tsx
+++ 9_exercises/src/components/ShoppingList.tsx
@@ -10,6 +10,7 @@ const ShoppingList = ({ products }: ProductsList) => {
   return (
     <>
       <h1>Exercício 2</h1>
+      <h2>Lista de produtos</h2>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
