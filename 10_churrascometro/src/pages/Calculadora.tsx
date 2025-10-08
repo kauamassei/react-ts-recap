306b7be742213699ddeccb8b880d7266bad76501
kauamassei <gameplayultra11@gmail.com>
2025-10-08 01:52:54 -0300

    feat: add components pages

---
+++ 10_churrascometro/src/pages/Calculadora.tsx
@@ -0,0 +1,12 @@
+import CalculadoraChurrasco from "../components/CalculadoraChurrasco";
+
+const Calculadora = () => {
+  return (
+    <>
+      <h1>Calculadora de Churrasco</h1>
+      <CalculadoraChurrasco />
+    </>
+  );
+};
+
+export default Calculadora;
---
+++ 10_churrascometro/src/types.ts
@@ -0,0 +1,16 @@
+export type Alimento = "carne" | "linguica" | "frango" | "paoAlho";
+
+export const quantidadePessoas: Record<Alimento, number> = {
+  carne: 400,
+  linguica: 200,
+  frango: 200,
+  paoAlho: 100,
+};
+
+export const nomeAlimentos: Record<string, string> = {
+  carne: "Carne",
+  linguica: "Linguica",
+  frango: "Frango",
+  paoAlho: "Pão de Alho",
+};
+
