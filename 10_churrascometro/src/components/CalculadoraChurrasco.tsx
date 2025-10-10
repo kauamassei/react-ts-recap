c40b661f32a05b4f123a6a2c39d000bdca63585c
kauamassei <gameplayultra11@gmail.com>
2025-10-10 00:29:37 -0300

    feat: add form cand routes

--- 10_churrascometro/src/components/CalculadoraChurrasco.tsx
+++ 10_churrascometro/src/components/CalculadoraChurrasco.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+import { nomeAlimentos } from "../types";
+
+import { Formik, Field, Form } from "formik";
+
+import * as Yup from "yup";
 
 const CalculadoraChurrasco = () => {
-  return (
-    <div>CalculadoraChurrasco</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default CalculadoraChurrasco
+  return (
+    <>
+      <div>
+        <Formik
+          onSubmit={() => {
+            navigate("/resultado");
+            console.log("Teste");
+          }}
+          initialValues={{ pessoas: 0, selecaoAlimentos: [] }}
+        >
+          <Form>
+            <div>
+              <label htmlFor="pessoas">Número de pessoas</label>
+              <Field name="pessoas" type="number" />
+            </div>
+            <h2>Selecione os alimentos para o churrasco:</h2>
+            {Object.keys(nomeAlimentos).map((alimento) => (
+              <div>
+                <Field
+                  type="checkbox"
+                  name="selecaoAlimentos"
+                  value={alimento}
+                />
+                <label htmlFor="selecaoAlimentos">
+                  {nomeAlimentos[alimento]}
+                </label>
+              </div>
+            ))}
+            <button type="submit">Calcular</button>
+          </Form>
+        </Formik>
+      </div>
+    </>
+  );
+};
+
+export default CalculadoraChurrasco;
