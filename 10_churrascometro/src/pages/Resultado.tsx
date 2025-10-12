31677aa365d3861f540193eb96810ac3efaec614
kauamassei <gameplayultra11@gmail.com>
2025-10-12 01:29:51 -0300

    styles: add result page css

--- 10_churrascometro/src/pages/Resultado.tsx
+++ 10_churrascometro/src/pages/Resultado.tsx
@@ -1,8 +1,11 @@
 import ResultadoChurrasco from "../components/ResultadoChurrasco";
-
+import bgImage from "../assets/bg-2.jpg";
 const Resultado = () => {
   return (
-    <div className="page-container">
+    <div
+      className="page-container"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       <h1>Resultado</h1>Resultado
       <ResultadoChurrasco />
     </div>
