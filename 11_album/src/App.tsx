e327758512952e7386db09f07f9b744d82367e91
kauamassei <gameplayultra11@gmail.com>
2025-10-13 23:41:14 -0300

    feat: add request api

--- 11_album/src/App.tsx
+++ 11_album/src/App.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
 import FotoAmpliada from "./components/FotoAmpliada";
 import FotoList from "./components/FotoList";
 import SearchBar from "./components/SearchBar";
+import axios from "axios";
+
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [category, setCategory] = useState("");
+
+  const fetchData = async () => {
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
+
+    const response = await axios.get("https://api.unsplash.com/photos/random", {
+      params: {
+        client_id: apiKey,
+      },
+    });
+
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="container">
