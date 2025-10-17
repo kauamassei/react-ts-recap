73addd400f5ee4c0bd7d8305b8232b11ddd7542e
kauamassei <gameplayultra11@gmail.com>
2025-10-17 01:20:59 -0300

    style: add search styles

--- 12_github_finder/src/App.tsx
+++ 12_github_finder/src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import classes from "./App.module.css";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className={classes.app}>
       <h1>GitHub Finder</h1>
       <Outlet />
     </div>
