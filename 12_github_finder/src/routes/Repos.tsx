548acdb946c6f17d2433935b6202fa715bd310af
kauamassei <gameplayultra11@gmail.com>
2025-10-21 01:42:03 -0300

    feat: add repo page

--- 12_github_finder/src/routes/Repos.tsx
+++ 12_github_finder/src/routes/Repos.tsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 import classes from "../components/Repos.module.css";
 import Loader from "../components/Loader";
 import BackBtn from "../components/BackBtn";
-
+import Repo from "../components/Repo";
 
 const Repos = () => {
   const { username } = useParams();
@@ -16,24 +16,39 @@ const Repos = () => {
 
   useEffect(() => {
     const loadRepos = async function (username: string) {
-        setIsLoading(true);
+      setIsLoading(true);
 
-        const res = await fetch(`https://api.github.com/users/${username}/repos`);
+      const res = await fetch(`https://api.github.com/users/${username}/repos`);
 
-        const data = await res.json();
+      const data = await res.json();
 
-        setIsLoading(false)
-        console.log(data)
+      setIsLoading(false);
+
+      setRepos(data);
+    };
+
+    if (username) {
+      loadRepos(username);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    loadRepos(username)
-  }, [])
-
+  if (!repos && isLoading) return <Loader />;
   return (
     <>
       <div>
         <BackBtn />
-        Repos: {username}
+        <h2>Explore os reposistórios do usuário: {username}</h2>
+        {repos && repos.length === 0 && <p>Não há repositórios.</p>}
+        {repos && repos.length > 0 && (
+          <div>
+            {repos.map((repo: RepoProps) => (
+              <div>
+                <Repo key={repo.name} {...repo} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
