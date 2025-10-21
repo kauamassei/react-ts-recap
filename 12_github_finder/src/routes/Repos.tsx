f5e5fa6157080cbecad5fd6c92ea0d5167a6ca20
kauamassei <gameplayultra11@gmail.com>
2025-10-21 01:15:52 -0300

    feat: add repositories request

--- 12_github_finder/src/routes/Repos.tsx
+++ 12_github_finder/src/routes/Repos.tsx
@@ -1,9 +1,34 @@
+import type { RepoProps } from "../types/repo";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
 import classes from "../components/Repos.module.css";
+import Loader from "../components/Loader";
 import BackBtn from "../components/BackBtn";
+
 
 const Repos = () => {
   const { username } = useParams();
+
+  const [repos, setRepos] = useState<RepoProps[] | [] | null>([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const loadRepos = async function (username: string) {
+        setIsLoading(true);
+
+        const res = await fetch(`https://api.github.com/users/${username}/repos`);
+
+        const data = await res.json();
+
+        setIsLoading(false)
+        console.log(data)
+    }
+
+    loadRepos(username)
+  }, [])
+
   return (
     <>
       <div>
