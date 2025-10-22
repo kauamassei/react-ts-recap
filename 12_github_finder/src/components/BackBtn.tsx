5ff3a41f380cfdf9d26a396871a0ee8e04549281
kauamassei <gameplayultra11@gmail.com>
2025-10-22 00:47:07 -0300

    style: add Repos page and back btn css

--- 12_github_finder/src/components/BackBtn.tsx
+++ 12_github_finder/src/components/BackBtn.tsx
@@ -1,8 +1,9 @@
 import { useNavigate } from "react-router-dom";
+import classes from './BackBtn.module.css'
 
 const BackBtn = () => {
   const navigate = useNavigate();
-  return <button onClick={() => navigate(-1)}>Voltar</button>;
+  return <button className={classes.back_btn} onClick={() => navigate(-1)}>Voltar</button>;
 };
 
 export default BackBtn;
--- 12_github_finder/src/routes/Repos.tsx
+++ 12_github_finder/src/routes/Repos.tsx
@@ -36,16 +36,14 @@ const Repos = () => {
   if (!repos && isLoading) return <Loader />;
   return (
     <>
-      <div>
+      <div className={classes.repos}>
         <BackBtn />
         <h2>Explore os reposistórios do usuário: {username}</h2>
         {repos && repos.length === 0 && <p>Não há repositórios.</p>}
         {repos && repos.length > 0 && (
-          <div>
+          <div className={classes.repos_container}>
             {repos.map((repo: RepoProps) => (
-              <div>
-                <Repo key={repo.name} {...repo} />
-              </div>
+              <Repo key={repo.name} {...repo} />
             ))}
           </div>
         )}
