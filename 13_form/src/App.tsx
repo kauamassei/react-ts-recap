e8f0900912f4ee998bb0d740e5f74e2546983399
kauamassei <gameplayultra11@gmail.com>
2025-10-26 02:09:36 -0300

    feat: add setup React form

---
+++ 13_form/src/App.tsx
@@ -0,0 +1,12 @@
+import Form from "./components/Form";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">React Hook Form com Typescript</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
