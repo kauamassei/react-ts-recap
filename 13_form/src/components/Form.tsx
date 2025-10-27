8398400de16c336ed0915b3b8a0af3b0d383a0e5
kauamassei <gameplayultra11@gmail.com>
2025-10-27 01:52:59 -0300

    feat: add initial input for some tests

--- 13_form/src/components/Form.tsx
+++ 13_form/src/components/Form.tsx
@@ -1,9 +1,28 @@
-
+import useForms from "../hooks/useForms";
+import { type FormSchema } from "../schemas/formSchema";
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+  const { register, handleSubmit, errors } = useForms();
 
-export default Form
+  const onSubmit = (data: FormSchema) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          id="name"
+          placeholder="Digite seu nome"
+          {...register("name")}
+        />
+        {errors.name && <small>{errors.name.message}</small>}
+      </div>
+      <button type="submit">Cadastrar</button>
+    </form>
+  );
+};
+
+export default Form;
