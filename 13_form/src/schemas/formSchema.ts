cd7d6532629cffd617c6beefd4c762c0de201e93
kauamassei <gameplayultra11@gmail.com>
2025-10-27 01:12:22 -0300

    feat: add form schema validations

---
+++ 13_form/src/schemas/formSchema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const formSchema = z
+  .object({
+    name: z.string().min(3, "O número mínimo de caracteres é 3."),
+    lastName: z.string().min(3, "O npumero mínimo de caracteres é 3."),
+    gender: z.string().refine((field) => field !== "select", {
+      message: "Você precisa scolher um gênero",
+    }),
+    email: z
+      .string()
+      .min(1, "O campo é obrigatório")
+      .email("Utilize um email válido"),
+    password: z.string().min(6, "A senha precisa ter no mínimo 6 caracteres"),
+    confirmPassword: z
+      .string()
+      .min(6, "A confirmacao precisa ter no mínimo 6 caracteres"),
+    agree: z.boolean().refine((field) => field === true, {
+      message: "Você precisa concordar com os termos.",
+    }),
+  })
+  .refine((field) => field.password === field.confirmPassword, {
+    message: "As senhas precisam ser iguais.",
+    path: ["confirmpassword"],
+  });
+
+export type FormSchema = z.infer<typeof formSchema>;
